89c6f2bf44f78163a518dfc393e4771e043c3e1e
taibihakim2002 <taibihakim2002@gmail.com>
2025-06-05 00:41:27 +0100

    fix cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://shifaouk.netlify.app',
     credentials: true,
 }));
 app.use(express.static("./uploads"))
