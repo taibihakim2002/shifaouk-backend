8ed005b73dcafcf3c94f7bf7624747fb6e835efe
taibihakim2002 <taibihakim2002@gmail.com>
2025-06-12 10:13:53 +0100

    change

--- routes/consultationReportRoutes.js
+++ routes/consultationReportRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { protect } = require("../controllers/authController");
-const { getReportByConsultationId, doctorCreateAppointmentReport } = require("../controllers/consultationReportController");
+const { getReportByConsultationId, doctorCreateAppointmentReport } = require("../controllers/ConsultationReportController");
 
 
 const router = express.Router()
